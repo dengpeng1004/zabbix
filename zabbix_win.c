5e34a6bf29a137ae681c3a8bc7cef471db9af8c5
dengpeng1004 <39081225+dengpeng1004@users.noreply.github.com>
2024-11-23 17:26:33 +0800

    Create zabbix_win.c

---
+++ zabbix_win.c
@@ -0,0 +1,13 @@
+1.编译链接 https://www.zabbix.com/documentation/6.0/zh/manual/installation/install/win_agent 不要使用 PCRE2,PCRE就行了
+
+2.错误处理: 2.1 ZABBIX_RC_NUM 修改 报错的 resource.rc 文件,要是图省事，直接改个日期就行 2.2 zabbix_sender_send_values 崩溃: common_log.c
+
+   void	zbx_log_handle(int level, const char *fmt, ...)
+   {
+    	va_list args;
+    
+    	va_start(args, fmt);
+    	if(log_func_callback) log_func_callback(level, fmt, args);
+    	va_end(args);
+    }
+3. 编译要使用VS2017 以上版本
